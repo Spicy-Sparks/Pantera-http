859653d19a21db00e5bbfb0a38e1a8d54845fe50
Alessandro Gerelli <geroale2000@gmail.com>
2025-11-07 18:20:42 +0100

    Fixes

--- dist/transform/url.js
+++ dist/transform/url.js
@@ -2,6 +2,11 @@ import { mergeUrl } from '../utils/config';
 export const transformUrl = (config) => {
     const finalUrl = mergeUrl(config.url || '', config.baseUrl);
     if (config.params && (Object.keys(config.params).length > 0)) {
+        for (const [key, value] of Object.entries(config.params)) {
+            if (typeof value === 'string') {
+                config.params[key] = encodeURIComponent(value);
+            }
+        }
         // @ts-ignore
         const urlSearchParams = new URLSearchParams(config.params).toString();
         return `${finalUrl}?${urlSearchParams}`;
--- src/transform/url.ts
+++ src/transform/url.ts
@@ -7,6 +7,11 @@ export const transformUrl = (
   const finalUrl = mergeUrl(config.url || '', config.baseUrl)
 
   if(config.params && (Object.keys(config.params).length > 0)) {
+    for (const [key, value] of Object.entries(config.params)) {
+      if (typeof value === 'string') {
+        config.params[key] = encodeURIComponent(value)
+      }
+    }
     // @ts-ignore
     const urlSearchParams = new URLSearchParams(config.params).toString()
     return `${finalUrl}?${urlSearchParams}`
